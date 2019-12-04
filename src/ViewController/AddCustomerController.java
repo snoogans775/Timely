c4834d90ffd64feb12b69ea861726f6e63169dcd
Kevin Fredericks <kfrednv@gmail.com>
2019-12-04 02:02:03 -0800

    Lambda functions for Exception handling in event controllers.

--- src/ViewController/AddCustomerController.java
+++ src/ViewController/AddCustomerController.java
@@ -5,23 +5,198 @@
  */
 package ViewController;
 
+import DB.Query;
+import DB.Session;
+import DB.mySQLConnection;
+import Model.*;
+import Model.User;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.time.Instant;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author kevin
- */
 public class AddCustomerController implements Initializable {
+    
+    @FXML TextField customerNameTextField;
+    @FXML TextField customerAddressTextField;
+    @FXML TextField customerAddressLineTwoTextField;
+    @FXML TextField customerPostalCodeTextField;
+    @FXML TextField customerPhoneTextField;
+    @FXML TextField customerCityTextField;
+    
+    @FXML ComboBox customerCountryComboBox;
+    @FXML CheckBox customerActiveCheckBox;
+    
+    //Get connection object for all other scenes
+    Connection conn = mySQLConnection.getConnection();
+    
+    Session session;
+    User currentUser;
 
-    /**
-     * Initializes the controller class.
-     */
+    //
+    //BEGIN METHODS
+    //
+    
+    //BEGIN CONTROL METHODS
+    
+    public void saveButtonPushed(ActionEvent event) throws SQLException, ParseException, IOException {
+        
+        //Get country from input
+        String countryString = customerCountryComboBox.getValue().toString();
+        Country countryObject = Query.getCountryByName( countryString, conn);
+        
+        //Add city
+        String city = customerCityTextField.getText();
+        
+        Query.addCity( new City(
+            0, //Placeholder cityid
+            customerCityTextField.getText(), //city
+            countryObject.getCountryid(),  //countryid
+            Timestamp.from( Instant.now() ),  //createDate
+            currentUser.getUserName(),  //createdBy
+            Timestamp.from( Instant.now()  ),  //lastUpdate
+            currentUser.getUserName()  //lastUpdateBy      
+
+        ), conn);
+        
+        City cityObject = Query.getCityByName(city, conn);
+        
+        //Add address      
+        String address = customerAddressTextField.getText();
+        Query.addAddress( new Address(
+            0, //All java objects have a placeholder primary id
+            address,
+            customerAddressLineTwoTextField.getText(),
+            cityObject.getCityid(), //CityId
+            customerPostalCodeTextField.getText(),
+            customerPhoneTextField.getText(),
+            Timestamp.from( Instant.now() ),
+            currentUser.getUserName(),
+            Timestamp.from( Instant.now()  ),
+            currentUser.getUserName()
+                
+        ), conn);
+        
+        //The addressid is AUTO_INCREMENT in the database.
+        //So the object id must be a get from the SQL database. 
+        Address addressObject = Query.getAddressByFirstLine( address, conn );
+        
+        //Create customer with address id
+        
+        //Convert adtive checkbox to int
+        Integer activeValue;
+        activeValue = customerActiveCheckBox.isSelected() ? 1 : 0;
+        
+        Customer customer = new Customer(
+            0,
+            customerNameTextField.getText(),
+            addressObject.getAddressid(),
+            activeValue,
+            Timestamp.from( Instant.now() ),
+            currentUser.getUserName(),
+            Timestamp.from( Instant.now()  ),
+            currentUser.getUserName()
+        );
+        
+        Query.addCustomer(customer, conn);
+        
+        //Change Scene to customer view
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation( getClass().getResource("/ViewController/Customer.fxml") );
+        Parent parent = loader.load();
+        Scene addParentScene = new Scene(parent);
+        
+        CustomerController controller = loader.getController();
+        controller.initSession( session );
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(addParentScene);
+        window.show();
+        
+    }
+    
+    public void cancelButtonPushed(ActionEvent event) throws IOException, SQLException {
+        
+        //Change Scene to customer view
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation( getClass().getResource("/ViewController/Customer.fxml") );
+        Parent parent = loader.load();
+        Scene addParentScene = new Scene(parent);
+        
+        CustomerController controller = loader.getController();
+        controller.initSession( session );
+
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(addParentScene);
+        window.show();
+        
+    }
+
+    //BEGIN DATA METHODS
+    
+    public Address getAddressFromInput() throws SQLException, ParseException {
+        
+        Address address = new Address(
+            0, //All java objects have a placeholder primary id
+            customerAddressTextField.getText(),
+            customerAddressLineTwoTextField.getText(),
+            0, //CityId
+            customerPostalCodeTextField.getText(),
+            customerPhoneTextField.getText(),
+            Timestamp.from( Instant.now() ),
+            currentUser.getUserName(),
+            Timestamp.from( Instant.now()  ),
+            currentUser.getUserName()
+        );
+        
+        return address;
+    }
+    
+    public ObservableList<String> generateCountryList() throws SQLException {
+        
+        List<String> countryList = Query.getAllCountryNames( conn );
+        
+        ObservableList<String> countryObservableList = FXCollections.observableArrayList(countryList);
+        
+        return countryObservableList;
+    }
+    
+    public void initSession(Session session) throws SQLException {
+
+        this.session = session;
+        this.currentUser = session.getCurrentUser();
+        
+    }
+    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        try {
+            customerCountryComboBox.setItems( generateCountryList() );
+        } catch (SQLException ex) {
+            Logger.getLogger(AddCustomerController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+
     }    
     
 }
